b35295f7dca1ee24900d9c4501240aea3ae85226
Andrew Burkett <burkett.andrew@gmail.com>
2015-11-24 12:35:01 -0800

    Initial Commit

--- src/StandardAtmosphere.jl
+++ src/StandardAtmosphere.jl
@@ -1,5 +1,73 @@
 module StandardAtmosphere
 
-# package code goes here
+export Atm
+
+using JSON
+using Base.Test
+import Base: show
+
+using MySIUnits
+import MySIUnits: SILength, SITemperature, SIPressure, SIDensity, SIVelocity, N, LengthUnit, TemperatureUnit
+
+immutable Atm
+    altitude::SILength{Float64}
+    delta_temperature::SITemperature{Float64}
+    temperature::SITemperature{Float64}
+    temperature_ratio::Float64
+    pressure::SIPressure{Float64}
+    pressure_ratio::Float64
+    density::SIDensity{Float64}
+    density_ratio::Float64
+    speed_of_sound::SIVelocity{Float64}
+end
+
+
+SeaLevelTemperature = 288.2K
+SeaLevelDensity = 1.225kg/m^3
+SeaLevelPressure = 101325N/m^2
+SeaLevelSpeedOfSound = 340.294m/s
+function Atm(altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+    if (altitude < 36089.24ft)
+        temperature_ratio = (1-altitude/145445.6ft);
+        pressure_ratio = temperature_ratio^5.2561;
+        density_ratio = temperature_ratio^4.2561;
+    else
+        temperature_ratio = 0.75187;
+        pressure_ratio = 0.22336*exp(-(altitude-36089.24ft)/20806.03ft);
+        density_ratio = pressure_ratio/0.75187;
+    end
+    delta_temperature_ratio = 1+delta_temperature/(temperature_ratio*SeaLevelTemperature);
+    temperature_ratio *= delta_temperature_ratio;
+    density_ratio /= delta_temperature_ratio;
+
+    temperature = temperature_ratio * SeaLevelTemperature;
+    density = density_ratio * SeaLevelDensity;
+    pressure = pressure_ratio * SeaLevelPressure;
+
+    speed_of_sound = SeaLevelSpeedOfSound*sqrt(pressure_ratio);
+    Atm(altitude,
+        delta_temperature,
+        temperature,
+        temperature_ratio,
+        pressure,
+        pressure_ratio,
+        density,
+        density_ratio,
+        speed_of_sound)
+end
+
+function show(io::IO,atm::Atm)
+    print(io,"Atm(")
+    print(io,"Altitude=",round(atm.altitude,1),", ")
+    print(io,"Temperature=",round(atm.temperature,1),", ")
+    print(io,"Pressure=",round(atm.pressure,1),", ")
+    print(io,"Density=",round(atm.density,3),")")
+end
+
+function Atm{N1<:Real,N2<:Real,U1<:LengthUnit,U2<:TemperatureUnit}(altitude::Quantity{N1,U1},delta_temperature::Quantity{N2,U2} = 0.0K)
+    Atm(convert(SIQuantity{Float64},altitude),convert(SIQuantity{Float64},delta_temperature))
+end
+
+include("env.jl")
 
 end # module
---
+++ src/env.jl
@@ -0,0 +1,73 @@
+export Env, kcas, keas, ktas, mach
+
+using StandardAtmosphere
+using MySIUnits
+using MySIUnits: SIVelocity, SIPressure, SILength, SITemperature, LengthUnit, TemperatureUnit
+
+import Base: *
+
+immutable AirspeedType{str} end
+kcas = AirspeedType{:kcas}()
+keas = AirspeedType{:keas}()
+ktas = AirspeedType{:ktas}()
+mach = AirspeedType{:mach}()
+
+type Airspeed{T}
+    val::Float64
+    Airspeed(v) = typeof(T)<:AirspeedType ? new{T}(v) : error("Invalid Airspeed Type")
+end
+
+show{T}(io::IO,::AirspeedType{T}) = print(io,T)
+show{T}(io::IO,a::Airspeed{T}) = print(io,a.val,T)
+*{T}(x::Real,t::AirspeedType{T}) = Airspeed{t}(x)
+
+immutable Env
+    atm::Atm
+    calibrated_airspeed::SIVelocity{Float64}
+    equivalent_airspeed::SIVelocity{Float64}
+    true_airspeed::SIVelocity{Float64}
+    mach_number::Float64
+    q::SIPressure{Float64}
+end
+
+function calc_calibrated(mach_number::Float64,pressure_ratio::Float64)
+     sqrt(5*((((0.2*mach_number^2+1)^3.5 - 1)*pressure_ratio+1)^(1/3.5) - 1))*SeaLevelSpeedOfSound
+end
+
+function calc_mach(calibrated_airspeed::SIVelocity{Float64},pressure_ratio::Float64)
+    sqrt(5*((((0.2*(calibrated_airspeed/SeaLevelSpeedOfSound)^2+1)^3.5 - 1)/pressure_ratio+1)^(1/3.5) - 1))
+end
+
+function Env(airspeed::Airspeed{kcas},altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+    atm = Atm(altitude,delta_temperature)
+    calibrated_airspeed = convert(SIQuantity,airspeed.val*knots)
+    mach_number = calc_mach(calibrated_airspeed,atm.pressure_ratio)
+    equivalent_airspeed = atm.speed_of_sound*mach_number
+    true_airspeed = equivalent_airspeed/sqrt(atm.density_ratio)
+    q = 0.5*atm.density*true_airspeed^2
+    Env(atm,calibrated_airspeed,equivalent_airspeed,true_airspeed,mach_number,q)
+end
+
+function Env(airspeed::Airspeed{mach},altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+    atm = Atm(altitude,delta_temperature)
+    mach_number = airspeed.val
+    calibrated_airspeed = calc_calibrated(mach_number,atm.pressure_ratio)
+    equivalent_airspeed = atm.speed_of_sound*mach_number
+    true_airspeed = equivalent_airspeed/sqrt(atm.density_ratio)
+    q = 0.5*atm.density*true_airspeed^2
+    Env(atm,calibrated_airspeed,equivalent_airspeed,true_airspeed,mach_number,q)
+end
+
+function Env(airspeed::Airspeed{keas},altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+    atm = Atm(altitude,delta_temperature)
+    equivalent_Airspeed = convert(SIQuantity,airspeed.val*knots)
+    mach_number = equivalent_airspeed/atm.speed_of_sound
+    calibrated_airspeed = calc_calibrated(mach_number,atm.pressure_ratio)
+    true_airspeed = equivalent_airspeed/sqrt(atm.density_ratio)
+    q = 0.5*atm.density*true_airspeed^2
+    Env(atm,calibrated_airspeed,equivalent_airspeed,true_airspeed,mach_number,q)
+end
+
+function Env{N1<:Real,N2<:Real,U1<:LengthUnit,U2<:TemperatureUnit}(airspeed::Airspeed,altitude::Quantity{N1,U1},delta_temperature::Quantity{N2,U2} = 0.0K)
+    Env(airspeed,convert(SIQuantity{Float64},altitude),convert(SIQuantity{Float64},delta_temperature))
+end
