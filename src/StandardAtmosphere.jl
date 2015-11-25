8d2ac3d52eb678a4683c03998bdb74f8307b2300
Andrew Burkett <burkett.andrew@gmail.com>
2015-11-24 16:22:29 -0800

    Add in environment calcs

--- src/StandardAtmosphere.jl
+++ src/StandardAtmosphere.jl
@@ -7,73 +7,7 @@ import Base: show, *
 using SIUnits
 using SIUnits.ShortUnits
 
-immutable Atm
-    altitude::@quantity(Float64,Meter)
-    delta_temperature::@quantity(Float64,Kelvin)
-    temperature::@quantity(Float64,Kelvin)
-    temperature_ratio::Float64
-    pressure::@quantity(Float64,N/m^2)
-    pressure_ratio::Float64
-    density::@quantity(Float64,kg/m^3)
-    density_ratio::Float64
-    speed_of_sound::@quantity(Float64,m/s)
-end
-
-
-SeaLevelTemperature = 288.15K
-SeaLevelDensity = 1.225kg/m^3
-SeaLevelPressure = 101327.0N/m^2
-SeaLevelSpeedOfSound = 340.2979m/s
-LowerStratosphereTemperature = 216.649K
-LowerStratosphereTemperatureRatio = LowerStratosphereTemperature/SeaLevelTemperature
-LowerStratospherePressure = 22633.0N/m^2
-LowerStratospherePressureRatio = LowerStratospherePressure/SeaLevelPressure
-HydrostaticConstant = 34.163195K/m
-
-function Atm(altitude::@quantity(Float64,Meter),delta_temperature::@quantity(Float64,Kelvin) = 0.0K)
-    # http://www.pdas.com/programs/atmos.f90
-    if (altitude < 11000m)
-        temperature_ratio = 1 - altitude/11000m*(1-LowerStratosphereTemperatureRatio);
-        pressure_ratio = temperature_ratio^(HydroStaticConstant/(6.5K/m));
-    elseif altitude < 25000m
-        temperature_ratio = LowerStratosphereTemperatureRatio;
-        pressure_ratio = LowerStratospherePressureRatio*exp(-HydrostaticConstant*(altitude-11000m)/LowerStratosphereTemperature);
-    else
-        throw(ErrorException("Atmosphere above 25000m not supported"))
-    end
-    density_ratio = pressure_ratio/temperature_ratio;
-    delta_temperature_ratio = 1+delta_temperature/(temperature_ratio*SeaLevelTemperature);
-    temperature_ratio *= delta_temperature_ratio;
-    density_ratio /= delta_temperature_ratio;
-
-    temperature = temperature_ratio * SeaLevelTemperature;
-    density = density_ratio * SeaLevelDensity;
-    pressure = pressure_ratio * SeaLevelPressure;
-
-    speed_of_sound = SeaLevelSpeedOfSound*sqrt(pressure_ratio);
-    Atm(altitude,
-        delta_temperature,
-        temperature,
-        temperature_ratio,
-        pressure,
-        pressure_ratio,
-        density,
-        density_ratio,
-        speed_of_sound)
-end
-
-function show(io::IO,atm::Atm)
-    print(io,"Atm(")
-    print(io,"Altitude=",round(atm.altitude,1),", ")
-    print(io,"Temperature=",round(atm.temperature,1),", ")
-    print(io,"Pressure=",round(atm.pressure,1),", ")
-    print(io,"Density=",round(atm.density,3),")")
-end
-
-# function Atm{N1<:Real,N2<:Real,U1<:LengthUnit,U2<:TemperatureUnit}(altitude::Quantity{N1,U1},delta_temperature::Quantity{N2,U2} = 0.0K)
-#     Atm(convert(SIQuantity{Float64},altitude),convert(SIQuantity{Float64},delta_temperature))
-# end
-
-#include("env.jl")
+include("atm.jl")
+include("env.jl")
 
 end # module
--- src/env.jl
+++ src/env.jl
@@ -15,22 +15,22 @@ show{T}(io::IO,a::Airspeed{T}) = print(io,a.val,T)
 
 immutable Env
     atm::Atm
-    calibrated_airspeed::SIVelocity{Float64}
-    equivalent_airspeed::SIVelocity{Float64}
-    true_airspeed::SIVelocity{Float64}
+    calibrated_airspeed::@quantity(Float64,m/s)
+    equivalent_airspeed::@quantity(Float64,m/s)
+    true_airspeed::@quantity(Float64,m/s)
     mach_number::Float64
-    q::SIPressure{Float64}
+    q::@quantity(Float64,N/m^2)
 end
 
 function calc_calibrated(mach_number::Float64,pressure_ratio::Float64)
      sqrt(5*((((0.2*mach_number^2+1)^3.5 - 1)*pressure_ratio+1)^(1/3.5) - 1))*SeaLevelSpeedOfSound
 end
 
-function calc_mach(calibrated_airspeed::SIVelocity{Float64},pressure_ratio::Float64)
+function calc_mach(calibrated_airspeed::@quantity(Float64,m/s),pressure_ratio::Float64)
     sqrt(5*((((0.2*(calibrated_airspeed/SeaLevelSpeedOfSound)^2+1)^3.5 - 1)/pressure_ratio+1)^(1/3.5) - 1))
 end
 
-function Env(airspeed::Airspeed{kcas},altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+function Env(airspeed::Airspeed{kcas},altitude::@quantity(Float64,m),delta_temperature::@quantity(Float64,K) = 0.0K)
     atm = Atm(altitude,delta_temperature)
     calibrated_airspeed = convert(SIQuantity,airspeed.val*knots)
     mach_number = calc_mach(calibrated_airspeed,atm.pressure_ratio)
@@ -40,7 +40,7 @@ function Env(airspeed::Airspeed{kcas},altitude::SILength{Float64},delta_temperat
     Env(atm,calibrated_airspeed,equivalent_airspeed,true_airspeed,mach_number,q)
 end
 
-function Env(airspeed::Airspeed{mach},altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+function Env(airspeed::Airspeed{mach},altitude::@quantity(Float64,m),delta_temperature::@quantity(Float64,K) = 0.0K)
     atm = Atm(altitude,delta_temperature)
     mach_number = airspeed.val
     calibrated_airspeed = calc_calibrated(mach_number,atm.pressure_ratio)
@@ -50,7 +50,7 @@ function Env(airspeed::Airspeed{mach},altitude::SILength{Float64},delta_temperat
     Env(atm,calibrated_airspeed,equivalent_airspeed,true_airspeed,mach_number,q)
 end
 
-function Env(airspeed::Airspeed{keas},altitude::SILength{Float64},delta_temperature::SITemperature{Float64} = 0.0K)
+function Env(airspeed::Airspeed{keas},altitude::@quantity(Float64,m),delta_temperature::@quantity(Float64,K) = 0.0K)
     atm = Atm(altitude,delta_temperature)
     equivalent_Airspeed = convert(SIQuantity,airspeed.val*knots)
     mach_number = equivalent_airspeed/atm.speed_of_sound
@@ -60,6 +60,6 @@ function Env(airspeed::Airspeed{keas},altitude::SILength{Float64},delta_temperat
     Env(atm,calibrated_airspeed,equivalent_airspeed,true_airspeed,mach_number,q)
 end
 
-function Env{N1<:Real,N2<:Real,U1<:LengthUnit,U2<:TemperatureUnit}(airspeed::Airspeed,altitude::Quantity{N1,U1},delta_temperature::Quantity{N2,U2} = 0.0K)
-    Env(airspeed,convert(SIQuantity{Float64},altitude),convert(SIQuantity{Float64},delta_temperature))
-end
+# function Env{N1<:Real,N2<:Real,U1<:LengthUnit,U2<:TemperatureUnit}(airspeed::Airspeed,altitude::Quantity{N1,U1},delta_temperature::Quantity{N2,U2} = 0.0K)
+#     Env(airspeed,convert(SIQuantity{Float64},altitude),convert(SIQuantity{Float64},delta_temperature))
+# end
